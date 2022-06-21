0526a2b5365956bdbe1e99c2d6833f174f6461df
Noel Ramirez <rnoel2125@gmail.com>
2022-06-21 01:34:20 -0400

    Did some more styling work and removed the out of the park section and begun putting together the review input form.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 const displayArea = $("#review-display-area");
+const reviewForm = $("#review-form-area");
+$("#add-review-button").on("click", () => {
+    
+});
+/*
+    review format:
+    <div class="review">
+        <h3>{title}</h3>
+        conditional elements:
+        if paragraph:
+        <p>{content.text}</p>
+        if unordered list:
+        <ul>
+            <li>{content.text{i}[0]}</li>
+            <li>{content.text{i}[1]}</li>
+            etc...
+        </ul>
+        if ordered list:
+        <ol type={olType}>
+            <li>{content.text{i}[0]}</li>
+            <li>content.text{i}[1]</li>
+            etc...
+        </ol>
+    </div>
+    review data object:
+    title,
+    content {
+        text0: "" or []: "", "", etc...,
+        if text == []: type0: "unordered" or "ordered"
+        text1: "" or []: "", "", etc...,
+
+    }
+*/
+
+function generateReview () {
+    
+}
