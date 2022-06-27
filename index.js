97cd1fc91984c11a2257ffb147a4e72b465347ea
Noel <rnoel2125@gmail.com>
2022-06-26 21:45:56 -0400

    Have added more of the review form features.

--- index.js
+++ index.js
@@ -16,15 +16,41 @@ $("#review-content-plus-button").click((event) => {
     if (event.target.tagName !== "INPUT"){return;}
     generateContentForm();
 });
+
+const getCurrentIndex = (id) => {
+    let chars = id.split("");
+    return parseInt(chars[chars.length-1]);
+};
+
 const onTypeChange = (event) => {
-    function getCurrentIndex() {
-        let id = event.target.id;
-        let chars = id.split("");
-        return parseInt(chars[chars.length-1]);
-    }
     if (event.target.tagName !== "SELECT") {return;}
-    if (event.target.selectedIndex === currentContentType[getCurrentIndex()]){return;}
-    //Change appropriate form fields TODO
+    let index = getCurrentIndex(event.target.id);
+    if (event.target.selectedIndex === currentContentType[index]){return;}
+    // Generate new form content and replace previous content. Then update currentContentType
+    const content = generateFormContent(index, event.target.selectedIndex);
+    const contentArea = $("#review-content-area"+index);
+    contentArea.empty();
+    contentArea.append(content);
+    currentContentType[index] = event.target.selectedIndex;
+}
+
+const onAddListItem = (event) => {
+    if (event.target.tagName !== "INPUT") {return;}
+    const contentArea = $("#review-content-area" + getCurrentIndex(event.target.id));
+    const label = $("<label>");
+    const textField = $("<input>");
+    console.log(contentArea.children());
+    const index = getCurrentIndex(contentArea.children().last().prop("id")) + 1;
+    label.prop("for", `list-item${index}`);
+    label.text("Enter List Item Text: ")
+    textField.prop("type", "text");
+    textField.prop("id", `list-item${index}`);
+    textField.prop("name", `list-item${index}`);
+    contentArea.append([label, textField]);
+}
+
+const onSubtractListItem = (event) => {
+    if (event.target.tagName !== "INPUT") {return;}
 }
 /*
     review format:
@@ -55,7 +81,6 @@ const onTypeChange = (event) => {
 
     }
 */
-
 function generateReview () {
     
 }
@@ -68,6 +93,69 @@ function removeContentForm () {
     $(`#${contentForms[contentForms.length-1].prop("id")}`).remove();
     contentForms.pop();
     currentContentType.pop();
+}
+
+function generateFormContent (index, contentType) {
+    let output = [];
+    const p = () => {
+        const contentFieldLabel = $("<label>");
+        const textArea = $("<textarea>");
+        contentFieldLabel.prop("for", `review-content-field${index}`);
+        contentFieldLabel.text("Enter Paragraph Here: ");
+        textArea.prop("id", `review-content-field${index}`);
+        textArea.prop("name", `review-content-field${index}`);
+        textArea.prop("row", "5");
+        textArea.prop("cols", "40");
+        output.push(contentFieldLabel, textArea);
+    }
+
+    const list = () => {
+        // Create elements
+        const buttonContainer = $("<div>");
+        const addButton = $("<input>");
+        const minusButton = $("<input>");
+        const listItemLabel = $("<label>");
+        const listItemTextField = $("<input>");
+        // Add properties
+        buttonContainer.prop("id", "list-buttons");
+        addButton.prop("type", "button");
+        addButton.prop("class", "list-button");
+        addButton.prop("id", "list-add-button"+index);
+        addButton.prop("name", "list-add-button"+index);
+        addButton.prop("value", "+");
+        minusButton.prop("type", "button");
+        minusButton.prop("class", "list-button");
+        minusButton.prop("id", "list-minus-button"+index);
+        minusButton.prop("name", "list-minus-button"+index);
+        minusButton.prop("value", "-");
+        listItemLabel.prop("for", `list-item0`);
+        listItemLabel.text("Enter List Item Text: ")
+        listItemTextField.prop("type", "text");
+        listItemTextField.prop("id", `list-item0`);
+        listItemTextField.prop("name", `list-item0`);
+        // Add event listener
+        addButton.on("click", onAddListItem);
+        minusButton.on("click", onSubtractListItem);
+        // Append Elements
+        buttonContainer.append([addButton, minusButton]);
+        output.push(buttonContainer, listItemLabel, listItemTextField);
+    }
+
+    switch(contentType){
+        case 0:
+            p();
+            break;
+        case 1:
+            list();
+            break;
+        case 2:
+            list();
+            break;
+        default:
+            p();
+            break;
+    }
+    return output;
 }
 
 function generateContentForm () {
@@ -83,15 +171,14 @@ function generateContentForm () {
         "Paragraph",
         "Udordered List",
         "Ordered List",
-        "Enter Paragraph Here: "
     ]
     // create elements
     const container = $("<div>");
     const typeLabel = $("<label>");
     const typeSelect = $("<select>");
     const typeOptions = generateOptions();
-    const contentFieldLabel = $("<label>");
-    const textArea = $("<textarea>");
+    const contentArea = $("<div>");
+    const formContent = generateFormContent(0);
     // add properties
     const index = contentForms.length;
     container.prop("id", `review-content${index}`);
@@ -99,6 +186,7 @@ function generateContentForm () {
     typeLabel.text(labelText[0]);
     typeSelect.prop("name", `review-content-type${index}`);   
     typeSelect.prop("id", `review-content-type${index}`);
+    contentArea.prop("id", `review-content-area${index}`);
     typeOptions[0].prop("value", "p");
     typeOptions[0].prop("selected", true);
     currentContentType.push(0); // initializing array current content type
@@ -107,18 +195,12 @@ function generateContentForm () {
     typeOptions[1].text(labelText[2]);
     typeOptions[2].prop("value", "ol");
     typeOptions[2].text(labelText[3]);
-    contentFieldLabel.prop("for", `review-content-field${index}`);
-    contentFieldLabel.text(labelText[4]);
-    textArea.prop("id", `review-content-field${index}`);
-    textArea.prop("name", `review-content-field${index}`);
-    textArea.prop("row", "5");
-    textArea.prop("cols", "40");
     // add events
-    typeSelect.on("change", );
+    typeSelect.on("change", onTypeChange);
     // append elements to create element tree
     typeSelect.append(typeOptions);
-    container.append([typeLabel, typeSelect, contentFieldLabel, textArea]);
-    console.log(container)
+    contentArea.append(formContent);
+    container.append([typeLabel, typeSelect, contentArea]);
     reviewContentFormArea.append(container);
     contentForms.push(container);
 }
